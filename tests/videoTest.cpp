c6a032d7356a4cca365545ca64c2f08faeb17e53
illuminati945 <illuminati8200@gmail.com>
2019-12-26 10:45:52 +0200

    minor edits

--- tests/videoTest.cpp
+++ tests/videoTest.cpp
@@ -11,15 +11,14 @@
 #include <cppflowATRInterface/Object_Detection_Types.h>
 #include <utils/imgUtils.h>
 
-void doATRinference(cv::Mat inp1, OD::ObjectDetectionManager* atrManager);
+void doATRinference(cv::Mat inp1, OD::ObjectDetectionManager *atrManager);
 OD::ObjectDetectionManager *createAtrManager();
 
 int main()
 {
-  cv::Mat frame(2160,4096, CV_8UC3);
+  cv::Mat frame(2160, 4096, CV_8UC3);
   cv::VideoCapture cap(VIDEO_PATH);
   OD::ObjectDetectionManager *atrManager = createAtrManager();
-  
 
   // Check if camera opened successfully
   if (!cap.isOpened())
@@ -34,12 +33,12 @@ int main()
     frame.release();
     cap >> frame;
 
-    //send to tensorflow
-    doATRinference(frame, atrManager);
-
     // If the frame is empty, break immediately
     if (frame.empty())
       break;
+
+    //send to tensorflow
+    doATRinference(frame, atrManager);
 
     //resize but keep aspect ratio
     cv::resize(frame, frame, cv::Size(frame.size().width / 3, frame.size().height / 3));
@@ -66,10 +65,8 @@ int main()
   return 0;
 }
 
-void doATRinference(cv::Mat inp1, OD::ObjectDetectionManager* atrManager)
+void doATRinference(cv::Mat inp1, OD::ObjectDetectionManager *atrManager)
 {
-  
-
 
   //emulate buffer from TIF
   cout << " ***  Read tif image to rgb buffer  ***  " << endl;
@@ -109,8 +106,6 @@ void doATRinference(cv::Mat inp1, OD::ObjectDetectionManager* atrManager)
   //release buffer
   delete ptrTif;
 
-  
-
   //release OD_CycleInput
   delete ci;
 
@@ -120,7 +115,6 @@ void doATRinference(cv::Mat inp1, OD::ObjectDetectionManager* atrManager)
 
   //return *ret;
 }
-
 
 OD::ObjectDetectionManager *createAtrManager()
 {
@@ -140,7 +134,7 @@ OD::ObjectDetectionManager *createAtrManager()
   };
 
   // support data
-  OD_SupportData *supportData1 =  new OD_SupportData();
+  OD_SupportData *supportData1 = new OD_SupportData();
   *supportData1 = {
       H, W,                     //imageHeight//imageWidth
       e_OD_ColorImageType::RGB, //colorType;
@@ -152,16 +146,16 @@ OD::ObjectDetectionManager *createAtrManager()
 
   OD_InitParams *initParams1 = new OD_InitParams();
   (*initParams1) = {
-          //(char*)"/home/magshim/MB2/TrainedModels/faster_MB_140719_persons_sel4/frozen_390k/frozen_inference_graph.pb", //fails
-          //(char*)"tryTRT_humans.pb", //sometimes works
-          //(char*)"/home/magshim/MB2/TrainedModels/MB3_persons_likeBest1_default/frozen_378K/frozen_inference_graph.pb", //works
-          //(char*)"tryTRT_humans.pb", //sometimes OK, sometimes crashes the system
-          (char *)"graphs/frozen_inference_graph_humans.pb",
-          //  (char*)"tryTRT_all.pb", //Nope
-          // (char*)"/home/magshim/cppflowATR/frozen_inference_graph_all.pb",
-          100, // max number of items to be returned
-          *supportData1,
-          *mission1};
+      //(char*)"/home/magshim/MB2/TrainedModels/faster_MB_140719_persons_sel4/frozen_390k/frozen_inference_graph.pb", //fails
+      //(char*)"tryTRT_humans.pb", //sometimes works
+      //(char*)"/home/magshim/MB2/TrainedModels/MB3_persons_likeBest1_default/frozen_378K/frozen_inference_graph.pb", //works
+      //(char*)"tryTRT_humans.pb", //sometimes OK, sometimes crashes the system
+      (char *)"graphs/frozen_inference_graph_humans.pb",
+      //  (char*)"tryTRT_all.pb", //Nope
+      // (char*)"/home/magshim/cppflowATR/frozen_inference_graph_all.pb",
+      100, // max number of items to be returned
+      *supportData1,
+      *mission1};
 
   // Creation of ATR manager + new mission
   OD::ObjectDetectionManager *atrManager = OD::CreateObjectDetector(initParams1); //first mission
