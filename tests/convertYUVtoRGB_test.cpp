aa5f7176171db27bc8c107ca0094342d16fa608e
illuminati945 <illuminati8200@gmail.com>
2020-02-16 18:00:58 +0200

    Added test nv12 main

--- tests/convertYUVtoRGB_test.cpp
+++ tests/convertYUVtoRGB_test.cpp
@@ -7,6 +7,8 @@ using namespace std::chrono;
 
 void stackoverflow_YUV2RGB(void *yuvDataIn, void *rgbDataOut, int w, int h, int outNCh);
 void itay_YUV2RGB(char *raw, int width, int height, cv::Mat *outRGB);
+void nv12_rgb(char *raw, int height, int width, cv::Mat *outRGB);
+
 char *itay_readBytesFromFile(std::string filepath);
 
 typedef std::chrono::high_resolution_clock::time_point TimeVar;
@@ -22,42 +24,36 @@ double funcTime(F func, Args &&... args)
     return duration(timeNow() - t1);
 }
 
-
 void NewMain()
 {
     {
-    int h = 4056;
-    int w = 3040;
+        int h = 4056;
+        int w = 3040;
 
-    char *buf = fastParseRaw("media/00006160.raw");
-    cv::Mat rgb(cv::Size(h, w), CV_8UC3);
+        char *buf = fastParseRaw("media/00006160.raw");
+        cv::Mat rgb(cv::Size(h, w), CV_8UC3);
 
-    cv::Mat *myRGB = new cv::Mat(h, w, CV_8UC3);
-    std::vector<uint8_t> img_data(h * w * 2);
-    for (int i = 0; i < h * w * 2; i++) //TODO: without for loop
-        img_data[i] = buf[i];
+        cv::Mat *myRGB = new cv::Mat(h, w, CV_8UC3);
+        std::vector<uint8_t> img_data(h * w * 2);
+        for (int i = 0; i < h * w * 2; i++) //TODO: without for loop
+            img_data[i] = buf[i];
 
-    std::cout << "testing boris's convertYUV420toRGB function. " << std::endl;
-    std::cout << "time: " << funcTime(convertYUV420toRGB, img_data, h, w, myRGB) << " milliseconds" << std::endl;
-    std::cout << "Displaying results... press any key to continue" << std::endl;
-    namedWindow("Image RGB", WINDOW_NORMAL);
-    imshow("Image RGB", *myRGB);
-    waitKey();
+        std::cout << "testing boris's convertYUV420toRGB function. " << std::endl;
+        std::cout << "time: " << funcTime(convertYUV420toRGB, img_data, h, w, myRGB) << " milliseconds" << std::endl;
+        std::cout << "Displaying results... press any key to continue" << std::endl;
+        namedWindow("Image RGB", WINDOW_NORMAL);
+        imshow("Image RGB", *myRGB);
+        waitKey();
 
-    std::cout << "testing itay's YUV2RGB function. " << std::endl;
-    std::cout << "time: " << funcTime(fastYUV2RGB, buf, h, w, &rgb) << " milliseconds" << std::endl;
-    std::cout << "Displaying results... press any key to continue" << std::endl;
+        std::cout << "testing itay's YUV2RGB function. " << std::endl;
+        std::cout << "time: " << funcTime(fastYUV2RGB, buf, h, w, &rgb) << " milliseconds" << std::endl;
+        std::cout << "Displaying results... press any key to continue" << std::endl;
 
-    namedWindow("Image RGB", WINDOW_NORMAL);
-    imshow("Image RGB", rgb);
-    waitKey();
+        namedWindow("Image RGB", WINDOW_NORMAL);
+        imshow("Image RGB", rgb);
+        waitKey();
+    }
 }
-
-
-
-
-}
-
 
 void ItayMain()
 {
@@ -85,6 +81,21 @@ void ItayMain()
 
     namedWindow("Image RGB", WINDOW_NORMAL);
     imshow("Image RGB", rgb);
+    waitKey();
+}
+
+void nv12main()
+{
+    int w = 4056;
+    int h = 3040;
+
+    char *buf = itay_readBytesFromFile("media/nv12/00000920.raw");
+    cv::Mat *rgb = new cv::Mat();
+    
+    nv12_rgb(buf, h, w, rgb);
+
+    namedWindow("Image RGB", WINDOW_NORMAL);
+    imshow("Image RGB", *rgb);
     waitKey();
 }
 
@@ -116,9 +127,15 @@ void itay_YUV2RGB(char *raw, int height, int width, cv::Mat *outRGB)
     cvtColor(yuyv442, *outRGB, COLOR_YUV2RGB_YUYV);
 }
 
+void nv12_rgb(char *raw, int height, int width, cv::Mat *outRGB)
+{
+    cv::Mat nv12(height * 3/2, width, CV_8UC1, raw);
+    cvtColor(nv12, *outRGB, CV_YUV2RGB_YV12);
+}
+
 int main(int argc, char const *argv[])
 {
-    NewMain();
+    nv12main();
 
     return 0;
 }
