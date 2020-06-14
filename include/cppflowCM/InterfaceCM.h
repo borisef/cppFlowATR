384059a745837bb6b20c5d22060832f00519268f
ep <ep@no.mail>
2020-06-14 11:24:36 +0300

    Modified InterfaceCM class to use InterfaceCMbace
    Use newly introduced interfaceCMbace abstract class as a base class for
    InterfaceCM without functionallity changes.

--- include/cppflowCM/InterfaceCM.h
+++ include/cppflowCM/InterfaceCM.h
@@ -34,6 +34,5 @@ public:
   std::vector<float> RunRGBImgPath(const unsigned char *ptr);
   std::vector<float> RunImgBB(cv::Mat img, OD::OD_BoundingBox bb);
   bool RunImgWithCycleOutput(cv::Mat img, OD::OD_CycleOutput *co, int startInd, int stopInd, bool copyResults = true);
-  OD::e_OD_TargetColor TargetColor(uint cid);
   void IdleRun();
 };
